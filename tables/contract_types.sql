6b106200867416be3179c22b5ac180bb697de68e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-12 07:36:00 +0100

    The column contract_type _seems_ (?) to be unique.

--- tables/contract_types.sql
+++ tables/contract_types.sql
@@ -3,5 +3,6 @@
 -- customer who has commitment in their contracts ( contract_category = 7,
 -- indicates the category of the contract type: commitment)
 --
+-- The column contract_type _seems_ (?) to be unique.
 
 select * from contract_types;
