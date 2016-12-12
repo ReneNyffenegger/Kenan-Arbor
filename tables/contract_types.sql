5fa80d9acc007c60c6f2fd4c74d16231d5f5411d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-12 07:32:41 +0100

    + contract_types.sql

---
+++ tables/contract_types.sql
@@ -0,0 +1,7 @@
+--
+-- Provide the contract type, for example, this table can used to select the
+-- customer who has commitment in their contracts ( contract_category = 7,
+-- indicates the category of the contract type: commitment)
+--
+
+select * from contract_types;
