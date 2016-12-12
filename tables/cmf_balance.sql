49e15c2b9a8639e4fb24e1b5697409fced9234b9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-12 07:32:05 +0100

    + cmf_balance.sql

---
+++ tables/cmf_balance.sql
@@ -0,0 +1,7 @@
+-- customer Balance Details in the account Level
+-- 
+-- find the closed date of bill(whne the invoice Balance reaches Zero),total
+-- due, new charges, Balance due, paid date shown in the bill in an Aggregate
+-- Level
+
+select * from cmf_balance;
